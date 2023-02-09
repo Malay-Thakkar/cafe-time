5586b238d312dcae6da739809f46c8a1526bf8b6
Malay-Thakkar <78149426+Malay-Thakkar@users.noreply.github.com>
2023-02-09 19:19:58 +0530

    final commit

--- client/src/component/Navigationbar.js
+++ client/src/component/Navigationbar.js
@@ -24,9 +24,9 @@ import { getCart, storeCart } from './helper'
 
 
 const Navigationbar = () => {
-  
 
-  const [showadmin,setshowadmin]=useState(false);
+
+  const [isadmin, setisadmin] = useState(false);
   const token = localStorage.getItem('token');
   const [cart, setCart] = useState({});
   const [theme, settheme] = useState("light");
@@ -35,80 +35,80 @@ const Navigationbar = () => {
     <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
   </svg>);
 
-var decoded = jwt_decode(token);
-let role=decoded.role;
+  var decoded = jwt_decode(token);
+  let role = decoded.role;
 
-useEffect(() => {
-  if("admin" === role){
-    setshowadmin(true);
-  }
-  //Runs only on the first render
-}, [role]);
-
-
+  useEffect(() => {
+    if ("admin" === role) {
+      setisadmin(true);
+    }
+    //Runs only on the first render
+  }, [role]);
 
 
 
 
 
-const themeToggler = () => {
-  // theme === 'light' ? settheme('dark') : settheme("light");
-  if (theme === 'light') {
-    settheme('dark');
-    seticon(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-brightness-high-fill" viewBox="0 0 16 16">
-      <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-    </svg>
-    );
-  }
-  else {
-    settheme('light');
-
-    seticon(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-moon-stars-fill" viewBox="0 0 16 16">
-      <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
-      <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
-    </svg>)
-  }
-
-};
 
 
+  const themeToggler = () => {
+    // theme === 'light' ? settheme('dark') : settheme("light");
+    if (theme === 'light') {
+      settheme('dark');
+      seticon(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-brightness-high-fill" viewBox="0 0 16 16">
+        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+      </svg>
+      );
+    }
+    else {
+      settheme('light');
 
-useEffect(() => {
-  getCart().then(cart => {
-    setCart(JSON.parse(cart));
-  });
-}, []);
+      seticon(<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
+        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
+      </svg>)
+    }
 
-useEffect(() => {
-  storeCart(JSON.stringify(cart));
-}, [cart]);
-return (
-  <>
-    <ThemeProvider theme={theme === "light" ? lighttheme : darktheme}>
-      <GlobalStyle />
+  };
 
-      <Router>
-        <nav className='topnav sticky-top mx-auto flex items-center justify-between py-2 navbar navbar-expand-lg navbar-light'>
-          <Link className="ml-5 " to="/">
-            <img style={{ height: 50 }} src="logo.png" alt="logo" />
-          </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
 
-          <div className="collapse navbar-collapse justify-between" id="navbarSupportedContent">
-            <ul className="items-center text-black navbar-nav mr-auto spaceing">
-              { showadmin?<li><Link to="/dashboard">Dashboard</Link></li>:null}
-              <li className="ml-6"><Link to="/">Home</Link></li>
-              <li className="ml-6"><Link to="/product">Product</Link></li>
-              <li className="ml-6"><Link to="/me">Profile</Link></li>
-              <li className="ml-6"><Link to="/login">Login</Link></li>
-              <li className="ml-6">
-                <button onClick={() => themeToggler()}>
-                  {icon}
-                </button>
-              </li>
-              {/* <li className="ml-6 nav-item">
+
+  useEffect(() => {
+    getCart().then(cart => {
+      setCart(JSON.parse(cart));
+    });
+  }, []);
+
+  useEffect(() => {
+    storeCart(JSON.stringify(cart));
+  }, [cart]);
+  return (
+    <>
+      <ThemeProvider theme={theme === "light" ? lighttheme : darktheme}>
+        <GlobalStyle />
+
+        <Router>
+          <nav className='topnav sticky-top mx-auto flex items-center justify-between py-2 navbar navbar-expand-lg navbar-light'>
+            <Link className="ml-5 " to="/">
+              <img style={{ height: 50 }} src="logo.png" alt="logo" />
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse justify-between" id="navbarSupportedContent">
+              <ul className="items-center text-black navbar-nav mr-auto spaceing">
+                {isadmin ? <li><Link to="/dashboard">Dashboard</Link></li> : null}
+                <li className="ml-6"><Link to="/">Home</Link></li>
+                <li className="ml-6"><Link to="/product">Product</Link></li>
+                <li className="ml-6"><Link to="/me">Profile</Link></li>
+                <li className="ml-6"><Link to="/login">Login</Link></li>
+                <li className="ml-6">
+                  <button onClick={() => themeToggler()}>
+                    {icon}
+                  </button>
+                </li>
+                {/* <li className="ml-6 nav-item">
                 <div className="dropdown">
                   <button className="btn dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cup-hot" viewBox="0 0 16 16">
@@ -134,42 +134,43 @@ return (
                 </div>
               </li> */}
 
-              <li className="ml-6 mr-3">
-                <Link to="/cart">
-                  <div className='cartStyle'>
-                    <span>{cart.totalItems ? cart.totalItems : 0}</span>
-                    <img className='ml-2 mr-3' src="cart.svg" alt="cart-icon" />
-                  </div>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+                <li className="ml-6 mr-3">
+                  <Link to="/cart">
+                    <div className='cartStyle'>
+                      <span>{cart.totalItems ? cart.totalItems : 0}</span>
+                      <img className='ml-2 mr-3' src="cart.svg" alt="cart-icon" />
+                    </div>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
 
-        <CartContext.Provider value={{ cart, setCart }}>
+          <CartContext.Provider value={{ cart, setCart }}>
 
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/addproduct" element={<Addproduct />} />
-            <Route path="/updateproduct" element={<Updateproduct />} />
-            <Route path="/deleteproduct" element={<Deleteproduct />} />
-            <Route path="/me" element={<Profile />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/product" exeact element={<Product />} />
-            <Route path="/product/:_id" exeact element={<SingleProduct />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/Cart" element={<Cart />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
+            <Routes>
+              {isadmin ? <Route path="/dashboard" element={<Dashboard />} /> : null}
+              {isadmin ? <Route path="/addproduct" element={<Addproduct />} /> : null}
+              {isadmin ? <Route path="/updateproduct" element={<Updateproduct />} /> : null}
+              {isadmin ? <Route path="/deleteproduct" element={<Deleteproduct />} /> : null}
+             
+              <Route path="/me" element={<Profile />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/product" exeact element={<Product />} />
+              <Route path="/product/:_id" exeact element={<SingleProduct />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/Cart" element={<Cart />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
 
-        </CartContext.Provider>
-      </Router>
+          </CartContext.Provider>
+        </Router>
 
-    </ThemeProvider>
-  </>
+      </ThemeProvider>
+    </>
 
-)
+  )
 }
 
 export default Navigationbar;
